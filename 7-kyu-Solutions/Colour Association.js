b896533d7638a3f281e3589858dc2d34a8973f12
Anna Bogdanovich <bogdanovicanna4@gmail.com>
2022-08-18 22:39:15 +0300

    Add 7-kuy-Solutions

---
+++ 7-kyu-Solutions/Colour Association.js
@@ -0,0 +1,16 @@
+//Colour plays an important role in our lifes. Most of us like this colour better then another. User experience specialists believe that certain colours have certain psychological meanings for us.
+// You are given a 2D array, composed of a colour and its 'common' association in each array element. The function you will write needs to return the colour as 'key' and association as its 'value'.
+// For example:
+// var array = [["white", "goodness"], ...] //returns [{white: 'goodness'}, ...]
+
+function colourAssociation(array){
+    let arr = [];
+    for (let i =0; i < array.length; i++){
+        let obj = {}
+        obj[array[i][0]]=array[i][1];
+        arr.push(obj)
+    }
+    return arr;
+}
+
+const colourAssociation = array => array.map(([colour, association]) => {return {[colour]:association}});
---
+++ 7-kyu-Solutions/My Language Skills.js
@@ -0,0 +1,12 @@
+//You are given a dictionary/hash/object containing some languages and your test results in the given languages. Return the list of languages where your test score is at least 60, in descending order of the results.
+
+// Note: the scores will always be unique (so no duplicate values)
+
+// Examples
+// {"Java": 10, "Ruby": 80, "Python": 65}    -->  ["Ruby", "Python"]
+// {"Hindi": 60, "Dutch" : 93, "Greek": 71}  -->  ["Dutch", "Greek", "Hindi"]
+// {"C++": 50, "ASM": 10, "Haskell": 20}     -->  []
+
+function myLanguages(results) {
+    return Object.entries(results).filter((e)=>e[1]>=60).sort((a, b) => b[1]-a[1]).map((e) => e[0]);
+}
