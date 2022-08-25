39a246c7f90bb92e65d6bb5abd53f4f32204a956
Anna Bogdanovich <bogdanovicanna4@gmail.com>
2022-08-25 19:21:18 +0300

    Add 7-kuy-Solution

---
+++ 7-kyu-Solutions/Mumbling.js
@@ -0,0 +1,11 @@
+//This time no story, no theory. The examples below show you how to write function accum:
+// Examples:
+// accum("abcd") -> "A-Bb-Ccc-Dddd"
+// accum("RqaEzty") -> "R-Qq-Aaa-Eeee-Zzzzz-Tttttt-Yyyyyyy"
+// accum("cwAt") -> "C-Ww-Aaa-Tttt"
+// The parameter of accum is a string which includes only letters from a..z and A..Z.
+
+function accum(s) {
+    return s.split('').map(
+        (el,index) => el.toUpperCase() + el.toLowerCase().repeat(index) + '-').join('').slice(0,-1)
+}
